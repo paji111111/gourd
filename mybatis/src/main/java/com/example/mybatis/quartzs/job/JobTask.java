0983b4095cefede85c6c37b295c115abdad43e5a
liuzhixin02 <liuzhixin02@baidu.com>
2018-08-22 19:22:17 +0800

    init

---
+++ mybatis/src/main/java/com/example/mybatis/quartzs/job/JobTask.java
@@ -0,0 +1,6 @@
+package com.example.mybatis.quartzs.job;
+
+public class JobTask {
+
+
+}
---
+++ mybatis/src/main/java/com/example/mybatis/quartzs/job/JobTaskWorkFlow.java
@@ -0,0 +1,12 @@
+package com.example.mybatis.quartzs.job;
+
+public class JobTaskWorkFlow {
+    private Long id;
+    private String name;
+    private Integer status;
+    private Integer deleted;
+    private Long created;
+
+
+
+}
